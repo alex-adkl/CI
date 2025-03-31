f9bb0cfb097009b204c713f2776a3a4b85afaf27
alex <alexadkl@gmail.com>
2025-03-31 14:07:49 +0200

    test3

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -2,7 +2,7 @@ const test = require("node:test");
 const assert = require("node:assert");
 const { fizzbuzz } = require("./fizzbuzz.js");
 
-test("fizzbuzz(4)", () => {
+test("fizzbuzz(3)", (t) => {
   assert.strictEqual(fizzbuzz(3), "Fizz");
 });
 
